af71c9166772c78ded7c2ff1b00cc461dd020d80
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-08 21:45:16 +0100

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Maya Ghamloush on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
