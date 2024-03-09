320958316468e362ea9dc54caf1ffa3e6c9225ba
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-09 18:54:29 +0100

    Header view Design

--- Avocado/View/ContentView.swift
+++ Avocado/View/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Avocado Recepies")
-        }
-        .padding()
+        ScrollView(.vertical, showsIndicators: false){
+           VStack(alignment: .center, spacing: 20){
+               
+               ScrollView(.horizontal, showsIndicators: false) {
+                   HStack (alignment:.top, spacing: 0){
+                       HeaderView()
+                   }//Header
+              }//ScrollView
+                
+                
+                
+                //MARK: - Footer
+                VStack(alignment: .center, spacing: 20){
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.colorGreenAdaptive)
+                        .padding(8)
+                    Text("Everything you wanted to know about avocado but were too afraid to ask")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }//VSTACK
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }//vstack
+        }//SCROLLVIEWE
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .padding(0)
     }
 }
 
---
+++ Avocado/View/HeaderView.swift
@@ -0,0 +1,66 @@
+//
+//  HeaderView.swift
+//  Avocado
+//
+//  Created by Maya Ghamloush on 09/03/2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    //MARK: - PROPERTIES
+    
+    
+    @State private var showHeadline: Bool = false
+    var slideAnimation: Animation {
+        Animation.spring(response: 1.5, dampingFraction: 0.5, blendDuration: 0.5)
+            .speed(1)
+            .delay(0.25)
+    }
+    
+    var body: some View {
+        ZStack {
+            Image(.avocadoSlice1)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack(alignment:.top, spacing: 0) {
+                Rectangle()
+                    .fill(.colorGreenLight)
+                    .frame(width: 4)
+                
+                
+                VStack (alignment: .leading, spacing: 6){
+                    Text("Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                    Text("Avocados are a powerhouse ingredient at any meal for anyone.")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                }
+                .padding(.vertical,0)
+                .padding(.horizontal,20)
+                .frame(width: 281, height: 105, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .background(.colorBlackTransparetLight)
+            }//HSTACK
+            .frame(width: 285, height: 105, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .offset(x: -66, y: showHeadline ? 75 : 220)
+            .animation(slideAnimation)
+            .onAppear(perform: {
+                showHeadline.toggle()
+            })
+             
+        }//ZSTACK
+        .frame(width: 480, height: 320, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    HeaderView()
+}
